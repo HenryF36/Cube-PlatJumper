deff8fe08b59eaf4b80d89cae8f666dde4df51c7
Henry F <168481957+HenryF36@users.noreply.github.com>
2024-11-15 19:11:59 -0500

    Add Files
    First Stages added

---
+++ DieArea.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class DieArea : Area3D
+{
+	public override void _Ready()
+	{
+		// Connect the body_entered signal to a function
+		this.BodyEntered += OnBodyEntered;
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		// Check if the body that entered is the player
+		if (body is Player)
+		{
+			// Call a function when the player collides with Object4
+			GD.Print("Player collided with DieArea");
+			GetTree().ChangeSceneToFile("res://Died.tscn");
+			GD.Print("Scene Changed.");
+		}
+	}
+}
