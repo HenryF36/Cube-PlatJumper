50604b14ef0dddfbe44a97012dd7e81cd9af296c
Henry F <168481957+HenryF36@users.noreply.github.com>
2024-11-18 16:22:22 -0500

    Levels Unlocked Works! Ajusted Camera

--- Scripts/LevelSelect.cs
+++ Scripts/LevelSelect.cs
@@ -3,7 +3,8 @@ using System;
 
 public partial class LevelSelect : Control
 {
-
+	private Label _label;
+	private static int LevelsUnlkd = 1;
 
 	private void _on_l_1_pressed()
 	{
@@ -11,6 +12,16 @@ public partial class LevelSelect : Control
 		GetTree().ChangeSceneToFile("res://Screens/Level 1.tscn");
 		GD.Print("Scene Changed.");
 	}
+	
 
+	public override void _Ready()
+	{
+		
+		_label = GetNode<Label>("LevelsUnlLabel");
+		GD.Print("Curently says: " + _label.Text);
+		_label.Text = "Levels Unlocked: "+ LevelsUnlkd;
+		// TODO: This program has the levels unlocked thing, store it in a file latter so It will stay the same evry time it runs. Now it is using a static var.
+
+	}
 	
 }
