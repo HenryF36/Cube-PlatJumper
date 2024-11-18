6c156f83328daf3f6b1ff5994b9085aefe8a7ad0
Henry F <168481957+HenryF36@users.noreply.github.com>
2024-11-18 16:22:22 -0500

    Misc

--- Scripts/LevelSelect.cs
+++ Scripts/LevelSelect.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class LevelSelect : Control
 {
-
+	private Label _label;
 
 	private void _on_l_1_pressed()
 	{
@@ -11,6 +11,15 @@ public partial class LevelSelect : Control
 		GetTree().ChangeSceneToFile("res://Screens/Level 1.tscn");
 		GD.Print("Scene Changed.");
 	}
+	
 
+	public override void _Ready()
+	{
+		// Assuming the Label node is a child of the current node, you can access it using GetNode
+		_label = GetNode<Label>("LevelsUnlLabel"); // Replace with the correct node path
+		// Access and modify the text of the label
+		GD.Print("Curently says: " + _label.Text); // To print the current text
+		//_label.Text = "New Text"; // To change the text
+	}
 	
 }
