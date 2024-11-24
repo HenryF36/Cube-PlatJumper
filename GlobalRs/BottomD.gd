77cc41189c7fae2f5b06f3ed0c3c78b316f6bd13
Henry F <168481957+HenryF36@users.noreply.github.com>
2024-11-24 15:31:29 -0500

    Fixed Script for bottom undid previous problems

--- GlobalRs/BottomD.gd
+++ GlobalRs/BottomD.gd
@@ -1,15 +1,15 @@
 extends Node
-@export var Player : CharacterBody3D
+
+@export var Player: CharacterBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if Player == null:
+		print("Warning: Player is not assigned!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Player.global_transform.origin.y < -30:
-		print("Player collided with Bottom.")
+	if Player and Player.global_transform.origin.y < -30:
+		print("Player collided with the bottom.")
 		# Change the scene to the specified file
 		get_tree().change_scene_to_file("res://Menu/Died.tscn")
-	
