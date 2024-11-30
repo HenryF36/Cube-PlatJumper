88c3b93e170aa0b9b0c72c912c2efac426facada
HenryF36 <henryfabinski36@outlook.com>
2024-11-30 17:48:46 -0500

    Added P3-03-D's, Plus image. Moved P3 back

--- Level 2/P302S.gd
+++ Level 2/P302S.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var Player: CharacterBody3D 
 @export var P301I: StaticBody3D
+@export var P302S: Area3D
+@export var P30301D: Area3D
+@export var P30302D: Area3D
 
 func _ready() -> void:
 	if Player == null or P301I == null:
@@ -14,5 +17,8 @@ func _on_area_entered(body: Node) -> void:
 	if body == Player:
 		print("Player collided with P3-02.")
 		P301I.visible = true
+		P302S.visible = false
+		P30301D.visible = true
+		P30302D.visible = true
 	else:
 		print("Collision detected with an unexpected node." )
