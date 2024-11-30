6f4325ae4b0f6c0f47dea9b7dd512d28bb220095
Henry F <168481957+HenryF36@users.noreply.github.com>
2024-11-30 15:44:29 -0500

    Added Button

--- Level 2/P302S.gd
+++ Level 2/P302S.gd
@@ -6,10 +6,12 @@ extends Node
 func _ready() -> void:
 	if Player == null or P301I == null:
 		print("Player or P301I is not assigned. Please check the exported variables.")
+	else:
+		print("P3-02-S Good.")
 
 # Called when a body enters the area.
 func _on_area_entered(body: Node) -> void:
 	if body == Player:
 		print("Player collided with P3-02.")
 	else:
-		print("Collision detected with an unexpected node.")
+		print("Collision detected with an unexpected node." )
